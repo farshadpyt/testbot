52463e6a81fdaa4fbb90e644fa1f0463fb6779e7
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-02-23 20:40:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,13 @@ async def start_message(bot, message):
 
 
 
+GROUP = "testfarshadbot"
+WELCOME_MESSAGE = "enter your welcome message"
 
+@farshad.on_message(filters.chat(GROUP) & filters.new_chat_members)
+def welcome bot(client, message):
+
+      message.reply_text(WELCOME_MESSAGE)
 
 
 
