2f16805354cd7f36ed464f43e401fcedfd77b8d4
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-02-23 15:13:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from cgitb import text
+from random import random
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-
+import random
 farshad=Client(
     "pyrogram bot",
     bot_token="5211118841:AAGZAOcFQQKsCjBUFpkaHmkqRO-qwVFjPi0",
@@ -9,10 +10,20 @@ farshad=Client(
     api_hash="b0849a17c7dd35b96938591e3ea1caca"
 )
 
+
+ALL_PIC = [
+ "https://telegra.ph/file/e65116508df148b96111a.jpg",
+ "https://telegra.ph/file/1d4735f3285e7a2cc5b37.jpg",
+ "https://telegra.ph/file/647fee61578929d0089cf.jpg"
+]
+
+
+
+
 @farshad.on_message(filters.command("start"))
 async def start_message(bot, message):
     await message.reply_photo(
-        photo="https://telegra.ph/file/e65116508df148b96111a.jpg",
+        photo=random.choice(ALL_PIC),
         caption="hello welcome",
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton("OWNER", url="t.me/farshadck"),
