ea11a15167a07b94edb24c2d737d66be825161d4
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-02-23 14:09:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 
 farshad=Client(
     "pyrogram bot",
-    bot_token="bot token",
-    api_id="api id",
-    api_hash="api hash"
+    bot_token="5211118841:AAGZAOcFQQKsCjBUFpkaHmkqRO-qwVFjPi0",
+    api_id="6203583",
+    api_hash="b0849a17c7dd35b96938591e3ea1caca"
 )
 
 @farshad.on_message(filter.command("start"))
