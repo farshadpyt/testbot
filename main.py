c3ea4c52757bbffc9609c1772ca7a207bda7d074
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-02-28 11:41:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -123,7 +123,12 @@ async def info(bot, msg):
             await msg.reply(text, quote=True)
     
     
-    
+@farshad.on_message(filters.command(["stickerid"]))
+async def stickerid(bot, message):   
+    if message.reply_to_message.sticker:
+       await message.reply(f"**𝐘𝐨𝐮𝐫 𝐬𝐭𝐢𝐜𝐤𝐞𝐫𝐬 𝐢𝐝 𝐢𝐬**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
+    else: 
+       await message.reply("𝐇𝐦𝐦𝐦 𝐢𝐭'𝐬 𝐧𝐨𝐭 𝐚 𝐬𝐭𝐢𝐜𝐤𝐞𝐫...!!!")
     
     
 @farshad.on_message(filters.command("info"))    
