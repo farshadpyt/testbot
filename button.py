441df2a995e254471cdb7b8f3787f9dcaa9fd923
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-02-28 10:35:25 +0530

    Update button.py

--- button.py
+++ button.py
@@ -1,1 +1,13 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
+
+
+HELP_BUTTON = InlineKeyboardMarkup( [[
+       InlineKeyboardButton("𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐝", callback_data="id"),
+       InlineKeyboardButton("𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐧𝐟𝐨", callback_data="info")
+       ],[
+       InlineKeyboardButton("🏠 𝐇𝐨𝐦𝐞", callback_data="start"),
+       InlineKeyboardButton("⬇️ 𝐂𝐥𝐨𝐬𝐞", callback_data="close"),
+       InlineKeyboardButton("🤠 𝐀𝐛𝐨𝐮𝐭", callback_data="about")
+       ]]
+       )
